937103d1388aeca53990d8c8bee8cbbfc5d79fe4
David Meunier (HP-EliteBook) <david.meunier@inserm.fr>
2017-12-15 13:47:00 +0100

    find_packages for install with pip?

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
 setup(
     name = "graphpype",
     version = '0.0.1dev',
-    packages = ['graphpype'],
+    packages = find_packages(),
     author = "David Meunier",
     description = "Graph analysis for neuropycon (using nipype, and ephypype); based on previous packages dmgraphanalysis and then dmgraphanalysis_nodes and graphpype"   , 
     lisence='BSD 3',
     install_requires=['numpy>=1.3.0',
+                      #'nipype'
                       'configparser',
                       "pandas",
                       "matplotlib"]
