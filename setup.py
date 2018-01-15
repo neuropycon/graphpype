a2531d2d9f65aa81c56e524a225893bdbce2a3d7
David Meunier (HP-EliteBook) <david.meunier@inserm.fr>
2018-01-15 17:52:01 +0100

    correction nipype

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description = "Graph analysis for neuropycon (using nipype, and ephypype); based on previous packages dmgraphanalysis and then dmgraphanalysis_nodes and graphpype"   , 
     lisence='BSD 3',
     install_requires=['numpy>=1.3.0',
-                      'nipype==0.13'
+                      #'nipype==0.13',
                       'configparser',
                       "pandas",
                       'networkx==1.9',
