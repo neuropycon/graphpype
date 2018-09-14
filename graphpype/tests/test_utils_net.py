f1bbb01accc158116326cb6414844070909592a5
David Meunier <david_meunier_79@hotmail.fr>
2018-09-14 16:07:41 +0200

    OK for dev5, adding test_neuropycon_data + all previous test in test_utils_cor

--- graphpype/tests/test_utils_net.py
+++ graphpype/tests/test_utils_net.py
@@ -33,107 +33,8 @@ from graphpype.utils_net import (return_net_list, read_lol_file,
                                  get_degree_pos_values_from_info_nodes_file, get_degree_neg_values_from_info_nodes_file,
                                  export_Louvain_net_from_list)
 
-# test return_net_list from conmat to sparse format
-
-
-#def test_return_net_list():
-
-    #conmat = np.load(conmat_file)
-
-    #print(conmat.shape)
-
-    #int_factor = 1000
-
-    #list_conmat = return_net_list(conmat, int_factor)
-
-    #print(list_conmat.shape)
-
-    #assert list_conmat.shape[1] == 3, "Error, list_conmat should be format index_i, index_j value (sparse representation of graph) and have shape 3 instead of  {} ".format(
-        #list_conmat.shape[1])
-
-
-#def test_read_lol_file():
-
-    #community_vect = read_lol_file(lol_file)
-
-    #print(community_vect)
-
-
-#def test_read_Pajek_corres_nodes():
-
-    #corres = read_Pajek_corres_nodes(Pajek_net_file)
-
-    #print(corres)
-
-
-#def test_read_Pajek_corres_nodes_and_sparse_matrix():
-
-    #corres, sp = read_Pajek_corres_nodes_and_sparse_matrix(Pajek_net_file)
-
-    #print(corres)
-
-    #print(sp)
-
-
-#def test_compute_modular_matrix():
-
-    #corres, sp = read_Pajek_corres_nodes_and_sparse_matrix(Pajek_net_file)
-
-    #community_vect = read_lol_file(lol_file)
-
-    #mod_mat = compute_modular_matrix(sp, community_vect)
-
-    #print(mod_mat)
-
-    #print(np.unique(mod_mat))
-
-## Node properties
-
-
-#def test_get_strength_values_from_info_nodes_file():
-
-    #Strength = get_strength_values_from_info_nodes_file(info_nodes_file)
-
-    #print(Strength)
-
-
-#def test_get_strength_pos_values_from_info_nodes_file():
-
-    #Strength_Pos = get_strength_pos_values_from_info_nodes_file(
-        #info_nodes_file)
-
-    #print(Strength_Pos)
-
-
-#def test_get_strength_neg_values_from_info_nodes_file():
-
-    #Strength_Neg = get_strength_neg_values_from_info_nodes_file(
-        #info_nodes_file)
-
-    #print(Strength_Neg)
-
-
-#def test_get_degree_pos_values_from_info_nodes_file():
-
-    #Degree_Pos = get_degree_pos_values_from_info_nodes_file(info_nodes_file)
-
-    #print(Degree_Pos)
-
-
-#def test_get_degree_neg_values_from_info_nodes_file():
-
-    #Degree_Neg = get_degree_neg_values_from_info_nodes_file(info_nodes_file)
-
-    #print(Degree_Neg)
-
-## testing Louvain Traag file building (for sake of compatibility of older codes)
-
-
-def test_export_Louvain_net_from_list():
-
-    print("***************************************")
-    
-    print (data_path)
+################################################ test if neuropycon_data package and data are available
+def test_neuropycon_data():
     
     try:
         import neuropycon_data
@@ -142,18 +43,102 @@ def test_export_Louvain_net_from_list():
         print ("neuropycon_data not installed")
         
     assert os.path.exists(neuropycon_data.__path__[0]), "warning, could not find path {}, {}".format(neuropycon_data.__path__)
-    
-    print(os.listdir(neuropycon_data.__path__[0]))
-    
     assert os.path.exists(os.path.join(neuropycon_data.__path__[0],'data')), "warning, could not find path {}, {}".format(os.path.join(neuropycon_data.__path__[0],'data'),os.listdir(os.path.join(neuropycon_data.__path__[0])))
-    
-    print(os.listdir(os.path.join(neuropycon_data.__path__[0],'data')))
-    
     assert os.path.exists(os.path.join(neuropycon_data.__path__[0],'data','data_con')), "warning, could not find path {}, {}".format(data_path,os.listdir(os.path.join(neuropycon_data.__path__[0],'data')))
-
-    print(os.listdir(os.path.join(neuropycon_data.__path__[0],'data','data_con')))
-          
+   
     assert os.path.exists(coords_file), "warning, could not find path {}, {}".format(coords_file,os.listdir(data_path))
+
+# test return_net_list from conmat to sparse format
+def test_return_net_list():
+
+    conmat = np.load(conmat_file)
+
+    print(conmat.shape)
+
+    int_factor = 1000
+
+    list_conmat = return_net_list(conmat, int_factor)
+
+    print(list_conmat.shape)
+
+    assert list_conmat.shape[1] == 3, "Error, list_conmat should be format index_i, index_j value (sparse representation of graph) and have shape 3 instead of  {} ".format(
+        list_conmat.shape[1])
+
+
+def test_read_lol_file():
+
+    community_vect = read_lol_file(lol_file)
+
+    print(community_vect)
+
+
+def test_read_Pajek_corres_nodes():
+
+    corres = read_Pajek_corres_nodes(Pajek_net_file)
+
+    print(corres)
+
+
+def test_read_Pajek_corres_nodes_and_sparse_matrix():
+
+    corres, sp = read_Pajek_corres_nodes_and_sparse_matrix(Pajek_net_file)
+
+    print(corres)
+
+    print(sp)
+
+
+def test_compute_modular_matrix():
+
+    corres, sp = read_Pajek_corres_nodes_and_sparse_matrix(Pajek_net_file)
+
+    community_vect = read_lol_file(lol_file)
+
+    mod_mat = compute_modular_matrix(sp, community_vect)
+
+    print(mod_mat)
+
+    print(np.unique(mod_mat))
+
+################################################################## Node properties #########################################################
+def test_get_strength_values_from_info_nodes_file():
+
+    Strength = get_strength_values_from_info_nodes_file(info_nodes_file)
+
+    print(Strength)
+
+
+def test_get_strength_pos_values_from_info_nodes_file():
+
+    Strength_Pos = get_strength_pos_values_from_info_nodes_file(
+        info_nodes_file)
+
+    print(Strength_Pos)
+
+
+def test_get_strength_neg_values_from_info_nodes_file():
+
+    Strength_Neg = get_strength_neg_values_from_info_nodes_file(
+        info_nodes_file)
+
+    print(Strength_Neg)
+
+
+def test_get_degree_pos_values_from_info_nodes_file():
+
+    Degree_Pos = get_degree_pos_values_from_info_nodes_file(info_nodes_file)
+
+    print(Degree_Pos)
+
+
+def test_get_degree_neg_values_from_info_nodes_file():
+
+    Degree_Neg = get_degree_neg_values_from_info_nodes_file(info_nodes_file)
+
+    print(Degree_Neg)
+
+########################################################### testing Louvain Traag file building (for sake of compatibility of older codes)
+def test_export_Louvain_net_from_list():
 
     coords = np.loadtxt(coords_file)
 
@@ -166,16 +151,16 @@ def test_export_Louvain_net_from_list():
 
 if __name__ == '__main__':
 
-    # test_return_net_list() ## OK
-    # test_read_lol_file()
-    # test_read_Pajek_corres_nodes()
-    # test_read_Pajek_corres_nodes_and_sparse_matrix()
-    # test_compute_modular_matrix()
+    test_return_net_list() ## OK
+    test_read_lol_file()
+    test_read_Pajek_corres_nodes()
+    test_read_Pajek_corres_nodes_and_sparse_matrix()
+    test_compute_modular_matrix()
 
-    # test_get_strength_values_from_info_nodes_file()
-    # test_get_strength_pos_values_from_info_nodes_file()
-    # test_get_strength_neg_values_from_info_nodes_file()
-    # test_get_degree_pos_values_from_info_nodes_file()
-    # test_get_degree_neg_values_from_info_nodes_file()
+    test_get_strength_values_from_info_nodes_file()
+    test_get_strength_pos_values_from_info_nodes_file()
+    test_get_strength_neg_values_from_info_nodes_file()
+    test_get_degree_pos_values_from_info_nodes_file()
+    test_get_degree_neg_values_from_info_nodes_file()
 
     test_export_Louvain_net_from_list()
