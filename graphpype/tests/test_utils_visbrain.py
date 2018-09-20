6f224379fcac1b551982aff4ae985b1834963977
David Meunier <david_meunier_79@hotmail.fr>
2018-09-20 09:41:35 +0200

    docstring + decommented code

--- graphpype/tests/test_utils_visbrain.py
+++ graphpype/tests/test_utils_visbrain.py
@@ -6,36 +6,28 @@ import os
 
 import numpy as np
 
-#import neuropycon_data as nd
+import neuropycon_data as nd
 
-#data_path = os.path.join(nd.__path__[0], "data", "data_con")
+data_path = os.path.join(nd.__path__[0], "data", "data_con")
 
-#print(data_path)
+print(data_path)
 
-#conmat_file = os.path.join(data_path, "Z_cor_mat_resid_ts.npy")
+conmat_file = os.path.join(data_path, "Z_cor_mat_resid_ts.npy")
 
-#coords_file = os.path.join(data_path, "ROI_MNI_coords-Atlas.txt")
+coords_file = os.path.join(data_path, "ROI_MNI_coords-Atlas.txt")
 
-#lol_file = os.path.join(data_path, "data_graph", "Z_List.lol")
+lol_file = os.path.join(data_path, "data_graph", "Z_List.lol")
 
-#Z_list_file = os.path.join(data_path, "data_graph", "Z_List.txt")
+Z_list_file = os.path.join(data_path, "data_graph", "Z_List.txt")
 
-#Pajek_net_file = os.path.join(data_path, "data_graph", "Z_List.net")
-
-#info_nodes_file = os.path.join(
-    #data_path, "data_graph", "Z_List-info_nodes.txt")
-
-
-#from graphpype.utils_net import (return_net_list, read_lol_file,
-                                 #read_Pajek_corres_nodes, read_Pajek_corres_nodes_and_sparse_matrix,
-                                 #compute_modular_matrix, get_strength_values_from_info_nodes_file,
-                                 #get_strength_pos_values_from_info_nodes_file, get_strength_neg_values_from_info_nodes_file,
-                                 #get_degree_pos_values_from_info_nodes_file, get_degree_neg_values_from_info_nodes_file,
-                                 #export_Louvain_net_from_list)
+Pajek_net_file = os.path.join(data_path, "data_graph", "Z_List.net")
 
 ################################################ test if vispy and visbrain package are available
 
 def test_vispy():
+    """
+    testing if vispy is installed
+    """
     
     try:
         import vispy
@@ -44,7 +36,9 @@ def test_vispy():
         print ("vispy not installed")
         
 def test_visbrain():
-    
+    """
+    testing if vispy is installed
+    """
     try:
         import visbrain
         
