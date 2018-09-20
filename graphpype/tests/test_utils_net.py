4697d1112f9ca5a91448f4dbf489b3e9c937c52f
David Meunier <david_meunier_79@hotmail.fr>
2018-09-20 13:34:17 +0200

    docstrings + autopep8

--- graphpype/tests/test_utils_net.py
+++ graphpype/tests/test_utils_net.py
@@ -33,24 +33,37 @@ from graphpype.utils_net import (return_net_list, read_lol_file,
                                  get_degree_pos_values_from_info_nodes_file, get_degree_neg_values_from_info_nodes_file,
                                  export_Louvain_net_from_list)
 
-################################################ test if neuropycon_data package and data are available
+# test if neuropycon_data package and data are available
+
+
 def test_neuropycon_data():
-    
+    """
+    test if neuropycon_data is installed
+    """
+
     try:
         import neuropycon_data
-        
+
     except ImportError:
-        print ("neuropycon_data not installed")
-        
-    assert os.path.exists(neuropycon_data.__path__[0]), "warning, could not find path {}, {}".format(neuropycon_data.__path__)
-    assert os.path.exists(os.path.join(neuropycon_data.__path__[0],'data')), "warning, could not find path {}, {}".format(os.path.join(neuropycon_data.__path__[0],'data'),os.listdir(os.path.join(neuropycon_data.__path__[0])))
-    assert os.path.exists(os.path.join(neuropycon_data.__path__[0],'data','data_con')), "warning, could not find path {}, {}".format(data_path,os.listdir(os.path.join(neuropycon_data.__path__[0],'data')))
-   
-    assert os.path.exists(coords_file), "warning, could not find path {}, {}".format(coords_file,os.listdir(data_path))
+        print("neuropycon_data not installed")
+
+    assert os.path.exists(neuropycon_data.__path__[
+                          0]), "warning, could not find path {}, {}".format(neuropycon_data.__path__)
+    assert os.path.exists(os.path.join(neuropycon_data.__path__[0], 'data')), "warning, could not find path {}, {}".format(
+        os.path.join(neuropycon_data.__path__[0], 'data'), os.listdir(os.path.join(neuropycon_data.__path__[0])))
+    assert os.path.exists(os.path.join(neuropycon_data.__path__[0], 'data', 'data_con')), "warning, could not find path {}, {}".format(
+        data_path, os.listdir(os.path.join(neuropycon_data.__path__[0], 'data')))
+
+    assert os.path.exists(coords_file), "warning, could not find path {}, {}".format(
+        coords_file, os.listdir(data_path))
 
 # test return_net_list from conmat to sparse format
-def test_return_net_list():
 
+
+def test_return_net_list():
+    """
+    test transforming matrix as np.array to list of edges
+    """
     conmat = np.load(conmat_file)
 
     print(conmat.shape)
@@ -66,21 +79,27 @@ def test_return_net_list():
 
 
 def test_read_lol_file():
-
+    """
+    Test reading modular partition as radatools representation to a 
+    """
     community_vect = read_lol_file(lol_file)
 
     print(community_vect)
 
 
 def test_read_Pajek_corres_nodes():
-
+    """
+    Test reading corres node vector given a Pajek .net file
+    """
     corres = read_Pajek_corres_nodes(Pajek_net_file)
 
     print(corres)
 
 
 def test_read_Pajek_corres_nodes_and_sparse_matrix():
-
+    """
+    Test reading corres node vector and sparse graph representation given a Pajek .net file
+    """
     corres, sp = read_Pajek_corres_nodes_and_sparse_matrix(Pajek_net_file)
 
     print(corres)
@@ -89,7 +108,10 @@ def test_read_Pajek_corres_nodes_and_sparse_matrix():
 
 
 def test_compute_modular_matrix():
-
+    """
+    Test computing modular matrix where edges between nodes belonging to the same module are 
+    given the same value
+    """
     corres, sp = read_Pajek_corres_nodes_and_sparse_matrix(Pajek_net_file)
 
     community_vect = read_lol_file(lol_file)
@@ -101,15 +123,21 @@ def test_compute_modular_matrix():
     print(np.unique(mod_mat))
 
 ################################################################## Node properties #########################################################
-def test_get_strength_values_from_info_nodes_file():
 
+
+def test_get_strength_values_from_info_nodes_file():
+    """
+    Test get Strength vector in radatools info-nodes.txt file
+    """
     Strength = get_strength_values_from_info_nodes_file(info_nodes_file)
 
     print(Strength)
 
 
 def test_get_strength_pos_values_from_info_nodes_file():
-
+    """
+    Test get Strength_Pos vector in radatools info-nodes.txt file
+    """
     Strength_Pos = get_strength_pos_values_from_info_nodes_file(
         info_nodes_file)
 
@@ -117,7 +145,9 @@ def test_get_strength_pos_values_from_info_nodes_file():
 
 
 def test_get_strength_neg_values_from_info_nodes_file():
-
+    """
+    Test get Strength_Neg vector in radatools info-nodes.txt file
+    """
     Strength_Neg = get_strength_neg_values_from_info_nodes_file(
         info_nodes_file)
 
@@ -125,21 +155,29 @@ def test_get_strength_neg_values_from_info_nodes_file():
 
 
 def test_get_degree_pos_values_from_info_nodes_file():
-
+    """
+    Test get Degree_Pos vector in radatools info-nodes.txt file
+    """
     Degree_Pos = get_degree_pos_values_from_info_nodes_file(info_nodes_file)
 
     print(Degree_Pos)
 
 
 def test_get_degree_neg_values_from_info_nodes_file():
-
+    """
+    Test get Degree_Neg vector in radatools info-nodes.txt file
+    """
     Degree_Neg = get_degree_neg_values_from_info_nodes_file(info_nodes_file)
 
     print(Degree_Neg)
 
-########################################################### testing Louvain Traag file building (for sake of compatibility of older codes)
-def test_export_Louvain_net_from_list():
+# maybe to remove
 
+
+def test_export_Louvain_net_from_list():
+    """
+    testing Louvain Traag file building (for sake of compatibility of older codes)
+    """
     coords = np.loadtxt(coords_file)
 
     Z_list = np.loadtxt(Z_list_file)
@@ -151,7 +189,7 @@ def test_export_Louvain_net_from_list():
 
 if __name__ == '__main__':
 
-    test_return_net_list() ## OK
+    test_return_net_list()
     test_read_lol_file()
     test_read_Pajek_corres_nodes()
     test_read_Pajek_corres_nodes_and_sparse_matrix()
