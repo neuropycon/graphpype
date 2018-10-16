abde6e17d67a2af5a56b0dd2e533d3ef93fb56c8
David Meunier <david_meunier_79@hotmail.fr>
2018-10-16 13:26:22 +0200

    deleted everything related to igraph

--- graphpype/interfaces/__init__.py
+++ graphpype/interfaces/__init__.py
@@ -1,10 +1,1 @@
 from . import radatools
-
-import imp
-try:
-    imp.find_module('igraph')
-    from . import plot_igraph
-    can_plot_igraph = True
-
-except ImportError:
-    can_plot_igraph = False
