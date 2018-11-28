d07e288996bcec70636a68fce0d0970717cec829
annapasca <a.pascarella@iac.cnr.it>
2018-11-28 11:12:26 -0500

    modified init

--- graphpype/pipelines/__init__.py
+++ graphpype/pipelines/__init__.py
@@ -1,2 +1,10 @@
-# from .conmat_to_graph import create_pipeline_conmat_to_graph_density
-# from .nii_to_conmat import create_pipeline_nii_to_conmat
+from . import conmat_to_graph
+from .conmat_to_graph import create_pipeline_conmat_to_graph_density  # noqa
+from .conmat_to_graph import create_pipeline_conmat_to_graph_threshold  # noqa
+from .conmat_to_graph import create_pipeline_net_list_to_graph  # noqa
+from .nii_to_conmat import create_pipeline_nii_to_conmat  # noqa
+from .nii_to_conmat import create_pipeline_nii_to_conmat_seg_template  # noqa
+from .nii_to_conmat import create_pipeline_nii_to_conmat_simple  # noqa
+from .nii_to_conmat import create_pipeline_nii_to_subj_ROI  # noqa
+from .nii_to_conmat import create_pipeline_nii_to_weighted_conmat  # noqa
+from .intmat_to_graph import create_pipeline_intmat_to_graph_threshold  # noqa
