b8b7d8b234609e02378bd1ab31f5b2b350189774
David Meunier <david_meunier_79@hotmail.fr>
2019-06-18 12:16:40 +0200

    Make install

--- _downloads/c408898dc7b654b78c9005349545b8bd/plot_inv_ts_to_graph.py
+++ _downloads/c408898dc7b654b78c9005349545b8bd/plot_inv_ts_to_graph.py
@@ -191,17 +191,16 @@ main_workflow.config['execution'] = {'remove_unnecessary_outputs': 'false'}
 #main_workflow.run(plugin='MultiProc', plugin_args={'n_procs': 2})
 
 ##############################################################################
-# plotting modules and labels
+# plotting modules
 
-from graphpype.utils_visbrain import visu_graph_modules # noqa
+#from graphpype.utils_visbrain import visu_graph_modules # noqa
 
-labels_file = op.join(data_path, "label_names.txt")
-coords_file = op.join(data_path, "label_centroid.txt")
+#labels_file = op.join(data_path, "label_names.txt")
+#coords_file = op.join(data_path, "label_centroid.txt")
 
-from visbrain.objects import SceneObj, BrainObj # noqa
+#from visbrain.objects import SceneObj, BrainObj # noqa
 
-### white background
-#sc = SceneObj(size=(1000, 1000), bgcolor=(.1, .1, .1))
+#sc = SceneObj(size=(1000, 1000), bgcolor=(1,1,1))
 
 #for nf, freq_band_name in enumerate(freq_band_names):
     #res_path = op.join(
@@ -212,45 +211,19 @@ from visbrain.objects import SceneObj, BrainObj # noqa
     #lol_file = op.join(res_path, "community_rada", "Z_List.lol")
     #net_file = op.join(res_path, "prep_rada", "Z_List.net")
 
-    #b_obj = BrainObj("white", translucent=True)
+    #b_obj = BrainObj("B1", translucent=True)
     #sc.add_to_subplot(b_obj, row=nf, use_this_cam=True, rotate='left',
                       #title=("Module for {} band".format(freq_band_name)),
-                      #title_size=14, title_bold=True, title_color='white')
+                      #title_size=14, title_bold=True, title_color='black')
 
     #c_obj,s_obj = visu_graph_modules(lol_file=lol_file, net_file=net_file,
                                #coords_file=coords_file,
-                               #labels_file=labels_file, inter_modules=False)
+                               #inter_modules=False)
 
     #sc.add_to_subplot(c_obj, row=nf)
     #sc.add_to_subplot(s_obj, row=nf)
 
 #sc.preview()
-
-
-sc = SceneObj(size=(1000, 1000), bgcolor=(1,1,1))
-
-for nf, freq_band_name in enumerate(freq_band_names):
-    res_path = op.join(
-        data_path, graph_analysis_name,
-        "graph_den_pipe_den_"+str(con_den).replace(".", "_"),
-        "_freq_band_name_"+freq_band_name+"_subject_id_sub-0003")
-
-    lol_file = op.join(res_path, "community_rada", "Z_List.lol")
-    net_file = op.join(res_path, "prep_rada", "Z_List.net")
-
-    b_obj = BrainObj("B1", translucent=True)
-    sc.add_to_subplot(b_obj, row=nf, use_this_cam=True, rotate='left',
-                      title=("Module for {} band".format(freq_band_name)),
-                      title_size=14, title_bold=True, title_color='black')
-
-    c_obj,s_obj = visu_graph_modules(lol_file=lol_file, net_file=net_file,
-                               coords_file=coords_file,
-                               inter_modules=False)
-
-    sc.add_to_subplot(c_obj, row=nf)
-    sc.add_to_subplot(s_obj, row=nf)
-
-sc.preview()
 
 ###############################################################################
 # plotting modules and roles
@@ -261,37 +234,6 @@ labels_file = op.join(data_path, "label_names.txt")
 coords_file = op.join(data_path, "label_centroid.txt")
 
 from visbrain.objects import SceneObj, BrainObj # noqa
-
-### figure white mesh and black background
-
-#sc = SceneObj(size=(1000, 1000), bgcolor=(.1, .1, .1))
-
-#for nf, freq_band_name in enumerate(freq_band_names):
-    #res_path = op.join(
-        #data_path, graph_analysis_name,
-        #"graph_den_pipe_den_"+str(con_den).replace(".", "_"),
-        #"_freq_band_name_"+freq_band_name+"_subject_id_sub-0003")
-
-    #lol_file = op.join(res_path, "community_rada", "Z_List.lol")
-    #net_file = op.join(res_path, "prep_rada", "Z_List.net")
-    #roles_file = op.join(res_path, "node_roles", "node_roles.txt")
-
-    #b_obj = BrainObj("white", translucent=True)
-    #sc.add_to_subplot(b_obj, row=nf, use_this_cam=True, rotate='left',
-                      #title=("Module for {} band".format(freq_band_name)),
-                      #title_size=14, title_bold=True, title_color='white')
-
-    #c_obj,list_sources = visu_graph_modules_roles(
-        #lol_file=lol_file, net_file=net_file, roles_file=roles_file,
-        #coords_file=coords_file, inter_modules=True, default_size=10,
-        #hub_to_non_hub=3)
-
-    #sc.add_to_subplot(c_obj, row=nf)
-
-    #for source in list_sources:
-        #sc.add_to_subplot(source, row=nf)
-
-#sc.preview()
 
 sc = SceneObj(size=(1000, 1000), bgcolor=(1,1,1))
 
