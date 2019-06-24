e7b77c376488456e2de3c3f4eca158855dc13178
David Meunier <david_meunier_79@hotmail.fr>
2019-06-24 17:33:00 +0200

    Make install

--- _downloads/97b36fc9fb5622c5a9b3f531e50659f1/plot_nii_to_graph.py
+++ _downloads/97b36fc9fb5622c5a9b3f531e50659f1/plot_nii_to_graph.py
@@ -38,21 +38,18 @@ data_path = op.join(nd.__path__[0], "data", "data_nii")
 # nipype the directory in which to store the outputs.
 
 # workflow directory within the `base_dir`
-conmat_analysis_name = 'nii_to_graph'
+conmat_analysis_name = 'conmat'
 
-rwm_mask_file = op.join(data_path,"sub-test_mask-anatWM.nii")
-rcsf_mask_file = op.join(data_path,"sub-test_mask-anatCSF.nii")
-
-ROI_mask_file = op.join(data_path,"Atlas","indexed_mask-Atlas.nii")
-ROI_coords_file = op.join(data_path,"Atlas","ROI_coords-Atlas.txt")
-ROI_MNI_coords_file =op.join(data_path,"Atlas","ROI_MNI_coords-Atlas.txt")
-ROI_labels_file = op.join(data_path,"Atlas","ROI_labels-Atlas.txt")
+ROI_mask_file = op.join(data_path,"ROI_HCP","indexed_mask-ROI_HCP.nii")
+ROI_coords_file = op.join(data_path,"ROI_HCP","ROI_coords-ROI_HCP.txt")
+ROI_MNI_coords_file =op.join(data_path,"ROI_HCP","ROI_MNI_coords-ROI_HCP.txt")
+ROI_labels_file = op.join(data_path,"ROI_HCP","ROI_labels-ROI_HCP.txt")
 
 ###############################################################################
 # Then we create a node to pass input filenames to DataGrabber from nipype
 
-subject_ids = ['test']
-func_sessions = ['rs']
+subject_ids = ['01']
+func_sessions = ['rest']
 infosource = pe.Node(interface=IdentityInterface(
     fields=['subject_id','session']),
     name="infosource")
@@ -75,43 +72,67 @@ datasource = pe.Node(
 
 datasource = pe.Node(interface=nio.DataGrabber(
     infields=['subject_id','session'],
-    outfields= ['img_file']),
+    outfields= ['img_file','gm_anat_file','wm_anat_file','csf_anat_file']),
     name = 'datasource')
 
 datasource.inputs.base_directory = data_path
-datasource.inputs.template = 'sub-%s_task-%s_bold.nii'
+datasource.inputs.template = '%ssub-%s%s%s%s'
 datasource.inputs.template_args = dict(
-
-img_file=[['subject_id','session']],
-
+img_file=[["wr",'subject_id',"_task-",'session',"_bold.nii"]],
+gm_anat_file=[["wc1",'subject_id',"",'',"_T1w.nii"]],
+wm_anat_file=[["wc2",'subject_id',"",'',"_T1w.nii"]],
+csf_anat_file=[["wc3",'subject_id',"",'',"_T1w.nii"]],
+rp_file=[["rp_",'subject_id',"_task-",'session',"_bold.txt"]],
        )
+
 datasource.inputs.sort_filelist = True
 
 #0/0
+################################################################################
+## This parameter corrdesponds to the percentage of highest connections retains
+## for the analyses. con_den = 1.0 means a fully connected graphs (all edges
+## are present)
 
+#import json  # noqa
+#import pprint  # noqa
+
+#data_graph = json.load(open("params_graph.json"))
+#pprint.pprint({'graph parameters': data_graph})
+
+## density of the threshold
+#con_den = data_graph['con_den']
+
+## The optimisation sequence
+#radatools_optim = data_graph['radatools_optim']
+
+from graphpype.pipelines.nii_to_conmat import create_pipeline_nii_to_conmat # noqa
 from graphpype.pipelines.nii_to_conmat import create_pipeline_nii_to_conmat_seg_template # noqa
-
+from graphpype.pipelines.nii_to_conmat import create_pipeline_nii_to_subj_ROI #noqa
 
 main_workflow = pe.Workflow(name= conmat_analysis_name)
 main_workflow.base_dir = data_path
 
 conf_interval_prob = 0.05
 
-    ###### time series and correlations
-cor_workflow = create_pipeline_nii_to_conmat_seg_template(main_path =
-data_path, conf_interval_prob = conf_interval_prob)
+###### time series and correlations
+cor_wf = create_pipeline_nii_to_conmat(main_path=data_path, conf_interval_prob=conf_interval_prob, resample=True, background_val=0.0)
 
-main_workflow.connect(datasource,'img_file',
-cor_workflow,'inputnode.nii_4D_file')
-#main_workflow.connect(datasource, 'rp_file', cor_workflow,'inputnode.rp_file')
+#cor_wf = create_pipeline_nii_to_conmat_seg_template(main_path =
+#data_path, conf_interval_prob = conf_interval_prob)
 
-cor_workflow.inputs.inputnode.wm_anat_file = rwm_mask_file
-cor_workflow.inputs.inputnode.csf_anat_file = rcsf_mask_file
+#cor_wf = create_pipeline_nii_to_subj_ROI(main_path =
+#data_path, resample = True)
 
-cor_workflow.inputs.inputnode.ROI_mask_file = ROI_mask_file
-cor_workflow.inputs.inputnode.ROI_coords_file = ROI_coords_file
-cor_workflow.inputs.inputnode.ROI_MNI_coords_file = ROI_MNI_coords_file
-cor_workflow.inputs.inputnode.ROI_labels_file = ROI_labels_file
+main_workflow.connect(datasource, 'img_file', cor_wf, 'inputnode.nii_4D_file')
+main_workflow.connect(datasource, 'gm_anat_file', cor_wf, 'inputnode.gm_anat_file')
+main_workflow.connect(datasource, 'wm_anat_file', cor_wf, 'inputnode.wm_anat_file')
+main_workflow.connect(datasource, 'csf_anat_file', cor_wf, 'inputnode.csf_anat_file')
+main_workflow.connect(datasource, 'rp_file', cor_wf,'inputnode.rp_file')
+
+cor_wf.inputs.inputnode.ROI_mask_file = ROI_mask_file
+cor_wf.inputs.inputnode.ROI_coords_file = ROI_coords_file
+cor_wf.inputs.inputnode.ROI_MNI_coords_file = ROI_MNI_coords_file
+cor_wf.inputs.inputnode.ROI_labels_file = ROI_labels_file
 
 ###############################################################################
 # We then connect the nodes two at a time. We connect the output
@@ -119,8 +140,6 @@ cor_workflow.inputs.inputnode.ROI_labels_file = ROI_labels_file
 # So, these two nodes taken together can grab data.
 
 main_workflow.connect(infosource, 'subject_id', datasource, 'subject_id')
-
-
 main_workflow.connect(infosource, 'session', datasource, 'session')
 
 ###############################################################################
@@ -146,7 +165,7 @@ from graphpype.pipelines.conmat_to_graph import create_pipeline_conmat_to_graph_
 graph_workflow = create_pipeline_conmat_to_graph_density(
     data_path, con_den=con_den, optim_seq=radatools_optim)
 
-main_workflow.connect(cor_workflow, 'compute_conf_cor_mat.Z_conf_cor_mat_file',
+main_workflow.connect(cor_wf, 'compute_conf_cor_mat.Z_conf_cor_mat_file',
                       graph_workflow, "inputnode.conmat_file")
 
 ################################################################################
@@ -166,6 +185,10 @@ main_workflow.connect(cor_workflow, 'compute_conf_cor_mat.Z_conf_cor_mat_file',
 #plt.show()
 
 ###############################################################################
+# Displaying connectivity Figures
+
+
+###############################################################################
 # Finally, we are now ready to execute our workflow.
 main_workflow.config['execution'] = {'remove_unnecessary_outputs': 'false'}
 
@@ -176,88 +199,36 @@ main_workflow.config['execution'] = {'remove_unnecessary_outputs': 'false'}
 
 ################################################################################
 ## plotting
-import numpy as np
-
-#from visbrain.objects import BrainObj
-
-f_vert = op.join(data_path,'Atlas',
-   'Macaque.F99UA1.LEFT.FLAT.CartSTD.Std-MESH.clean.73730.coord')
-f_face =op.join(data_path,'Atlas',
-    'Macaque.F99UA1.LEFT.FLAT.CartSTD.Std-MESH.clean.73730.topo')
-
-vert = np.genfromtxt(f_vert, skip_header=12, usecols=(1, 2, 3))
-faces = np.genfromtxt(f_face, skip_header=11).astype(int)
 
 
-#from graphpype.utils_visbrain import visu_graph_modules_roles
 
-from graphpype.utils_visbrain import visu_graph_modules # noqa
+from graphpype.utils_visbrain import visu_graph_modules
 
-res_path = op.join(data_path,conmat_analysis_name,
-    "graph_den_pipe_den_0_1",'_session_rs_subject_id_test')
-
-lol_file = op.join(res_path,"community_rada","Z_List.lol")
-
-print(lol_file)
-
-net_file = op.join(res_path,"prep_rada","Z_List.net")
-#roles_file = os.path.join(res_path,"node_roles","node_roles.txt")
+#labels_file = op.join(data_path, "label_names.txt")
+#coords_file = op.join(data_path, "label_centroid.txt")
 
 from visbrain.objects import SceneObj, BrainObj # noqa
 
-b_obj = BrainObj('monkey', vertices=vert, faces=faces, translucent=False)
+sc = SceneObj(size=(500, 500), bgcolor=(1,1,1))
 
-sc = SceneObj(size=(1000, 1000), bgcolor=(.1, .1, .1))
+res_path = op.join(
+    data_path, conmat_analysis_name,
+    "graph_den_pipe_den_"+str(con_den).replace(".", "_"),
+    "_session_rest_subject_id_01")
 
-sc.add_to_subplot(
-    b_obj, row=0, use_this_cam=True, rotate='left',
-    title="Module",
-    title_size=14, title_bold=True, title_color='white')
+lol_file = op.join(res_path, "community_rada", "Z_List.lol")
+net_file = op.join(res_path, "prep_rada", "Z_List.net")
+
+b_obj = BrainObj("B1", translucent=True)
+sc.add_to_subplot(b_obj, row=0, use_this_cam=True, rotate='left',
+                    title=("Modules"),
+                    title_size=14, title_bold=True, title_color='black')
 
 c_obj,s_obj = visu_graph_modules(lol_file=lol_file, net_file=net_file,
                             coords_file=ROI_MNI_coords_file,
-                            labels_file=ROI_labels_file, inter_modules=False,
-                            z_offset=+50)
+                            inter_modules=False)
 
 sc.add_to_subplot(c_obj, row=0)
 sc.add_to_subplot(s_obj, row=0)
 
 sc.preview()
-
-c_colval = {0:"lightblue",1:"red",2:"green",3:"yellow",4:"purple"}
-
-
-from graphpype.utils_visbrain import visu_graph_modules # noqa
-
-#labels_file = op.join(data_path, "correct_channel_names.txt")
-#coords_file = op.join(data_path, "MNI_coords.txt")
-
-#from visbrain.objects import SceneObj, BrainObj # noqa
-
-#sc = SceneObj(size=(1000, 1000), bgcolor=(.1, .1, .1))
-
-#for nf, freq_band_name in enumerate(freq_band_names):
-
-    #res_path = op.join(
-        #data_path, graph_analysis_name,
-        #"graph_den_pipe_den_"+str(con_den).replace(".", "_"),
-        #"_freq_band_name_"+freq_band_name)
-
-    #lol_file = op.join(res_path, "community_rada", "Z_List.lol")
-    #net_file = op.join(res_path, "prep_rada", "Z_List.net")
-
-    #b_obj = BrainObj("white", translucent=False)
-    #sc.add_to_subplot(
-        #b_obj, row=nf, use_this_cam=True, rotate='left',
-        #title=("Module for {} band".format(freq_band_name)),
-        #title_size=14, title_bold=True, title_color='white')
-
-    #c_obj,s_obj = visu_graph_modules(lol_file=lol_file, net_file=net_file,
-                               #coords_file=coords_file,
-                               #labels_file=labels_file, inter_modules=False,
-                               #z_offset=+50)
-    #sc.add_to_subplot(c_obj, row=nf)
-    #sc.add_to_subplot(s_obj, row=nf)
-
-
-#sc.preview()
