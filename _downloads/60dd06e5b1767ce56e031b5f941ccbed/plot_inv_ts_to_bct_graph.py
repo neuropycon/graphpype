65c9b264bc16840b09a4fd7cfe03e1396c2ee469
David Meunier <david_meunier_79@hotmail.fr>
2019-11-29 11:07:11 +0100

    Make install

--- _downloads/60dd06e5b1767ce56e031b5f941ccbed/plot_inv_ts_to_bct_graph.py
+++ _downloads/60dd06e5b1767ce56e031b5f941ccbed/plot_inv_ts_to_bct_graph.py
@@ -1,84 +1,102 @@
 """
+.. _inv_ts_to_bct_graph:
 
-.. _conmat_to_bct_graph:
+=========================================================
+Compute Graph properties from a given connectivity matrix
+=========================================================
+The inv_ts_to_graph pipeline performs spectral connectivity and graph analysis
+over time series. This workflow makes use of two chained pipelines, and
+requires both graphpype AND ephypype to be installed.
 
-==========================================================================
-Compute Graph properties from a given connectivity matrix with BCT toolbox
-==========================================================================
-The conmat_to_graph pipeline provide an example wrap of a function from the
-bctpy toolbox (Brain Connectivity Toolbox)
-<https://sites.google.com/site/bctnet/> as an alternative to the Radatools
-toolbox for graph metric computation.
-
-The **input** data should be a symetrical connecivity matrix in **npy** format.
+The **input** data should be a time series matrix in **npy** format.
 """
-
 # Authors: David Meunier <david_meunier_79@hotmail.fr>
-
 # License: BSD (3-clause)
 # sphinx_gallery_thumbnail_number = 2
 import os.path as op
-
 import nipype.pipeline.engine as pe
-
-from nipype.interfaces.utility import IdentityInterface
 import nipype.interfaces.io as nio
+
+from ephypype.nodes import create_iterator
+from ephypype.nodes import get_frequency_band
 
 ###############################################################################
 # Check if data are available
+
 from graphpype.utils_tests import load_test_data
 
-data_path = load_test_data("data_con_meg")
+data_path = load_test_data("data_inv_ts")
 
 ###############################################################################
-# This will be what we will loop on
-
-freq_band_names = ['alpha', 'beta']
-
-###############################################################################
-# Then, we create our workflow and specify the `base_dir` which tells
+# First, we create our workflow and specify the `base_dir` which tells
 # nipype the directory in which to store the outputs.
 
 # workflow directory within the `base_dir`
-graph_analysis_name = 'graph_analysis'
+graph_analysis_name = 'inv_ts_to_graph_analysis'
 
 main_workflow = pe.Workflow(name=graph_analysis_name)
 main_workflow.base_dir = data_path
 
 ###############################################################################
-# Then we create a node to pass input filenames to DataGrabber from nipype
-infosource = pe.Node(
-        interface=IdentityInterface(fields=['freq_band_name']),
-        name="infosource")
+# We now use a json file for describing the connectivity parameters, loaded
+# from a json as a dictionnary
 
-infosource.iterables = [('freq_band_name', freq_band_names)]
+import json  # noqa
+import pprint  # noqa
+
+data_con = json.load(open(op.join(op.dirname("__file__"),
+                                  "params_connectivity.json")))
+pprint.pprint({'connectivity parameters': data_con})
+
+freq_band_names = data_con['freq_band_names']
+freq_bands = data_con['freq_bands']
+
+# spectral_connectivity_parameters
+con_method = data_con['con_method']
+epoch_window_length = data_con['epoch_window_length']
+
+# sampling frequency
+sfreq = data_con['sfreq'] # When starting from raw MEG
+# (.fif) data, can be directly extracted from the file info
+
+frequency_node = get_frequency_band(freq_band_names, freq_bands)
+
+###############################################################################
+# Then we create a node to pass input filenames to DataGrabber from nipype
+
+subject_ids = ['sub-0003']  # 'sub-0004', 'sub-0006'
+infosource = create_iterator(['subject_id', 'freq_band_name'],
+                             [subject_ids, freq_band_names])
 
 ###############################################################################
 # and a node to grab data. The template_args in this node iterate upon
 # the values in the infosource node
 
-# template_path = '*%s/conmat_0_coh.npy'
-# template_args = [['freq_band_name']
-# datasource = create_datagrabber(data_path, template_path, template_args)
+template_path = '*%s_task-rest_run-01_meg_0_60_raw_filt_dsamp_ica_ROI_ts.npy'
 
 datasource = pe.Node(
-    interface=nio.DataGrabber(infields=['freq_band_name'],
-                              outfields=['conmat_file']),
+    interface=nio.DataGrabber(infields=['subject_id'], outfields=['ts_file']),
     name='datasource')
-datasource.inputs.base_directory = data_path
-datasource.inputs.template = ("%s/conmat_0_coh.npy")
-datasource.inputs.template_args = dict(
-    conmat_file=[['freq_band_name']])
 
+datasource.inputs.base_directory = data_path
+datasource.inputs.template = template_path
+
+datasource.inputs.template_args = dict(ts_file=[['subject_id']])
 datasource.inputs.sort_filelist = True
 
 ###############################################################################
-# This parameter corrdesponds to the percentage of highest connections retains
-# for the analyses. con_den = 1.0 means a fully connected graphs (all edges
-# are present)
+# We then use the pipeline used in the previous example :ref:`conmat_to_graph pipeline <conmat_to_graph>`
 
-import json  # noqa
-import pprint  # noqa
+from ephypype.pipelines import create_pipeline_time_series_to_spectral_connectivity # noqa
+
+spectral_workflow = create_pipeline_time_series_to_spectral_connectivity(
+    data_path, con_method=con_method,
+    epoch_window_length=epoch_window_length)
+
+
+###############################################################################
+# We now use a json file for describing the graph parameters, loaded
+# from a json as a dictionnary
 
 data_graph = json.load(open(op.join(op.dirname("__file__"),
                                   "params_bct_graph.json")))
@@ -99,10 +117,21 @@ graph_workflow = create_pipeline_bct_graph(
 # of the infosource node to the datasource node.
 # So, these two nodes taken together can grab data.
 
-main_workflow.connect(infosource, 'freq_band_name',
-                      datasource, 'freq_band_name')
+main_workflow.connect(infosource, 'subject_id',
+                      datasource, 'subject_id')
 
-main_workflow.connect(datasource, 'conmat_file',
+main_workflow.connect(infosource, 'freq_band_name',
+                      frequency_node, 'freq_band_name')
+
+main_workflow.connect(datasource, 'ts_file',
+                      spectral_workflow, "inputnode.ts_file")
+
+spectral_workflow.inputs.inputnode.sfreq = sfreq
+
+main_workflow.connect(frequency_node, 'freq_bands',
+                      spectral_workflow, 'inputnode.freq_band')
+
+main_workflow.connect(spectral_workflow, 'spectral.conmat_file',
                       graph_workflow, "inputnode.conmat_file")
 
 ###############################################################################
@@ -111,7 +140,7 @@ main_workflow.write_graph(graph2use='colored')  # colored
 
 ###############################################################################
 # and visualize it. Take a moment to pause and notice how the connections
-# here correspond to how we connected the nodes.
+# correspond to how we connected the nodes.
 
 #import matplotlib.pyplot as plt  # noqa
 #img = plt.imread(op.join(data_path, graph_analysis_name, 'graph.png'))
@@ -122,20 +151,20 @@ main_workflow.write_graph(graph2use='colored')  # colored
 
 ###############################################################################
 # Finally, we are now ready to execute our workflow.
+
 main_workflow.config['execution'] = {'remove_unnecessary_outputs': 'false'}
+main_workflow.run()
 
-# Run workflow locally on 2 CPUs
-main_workflow.run(plugin='MultiProc', plugin_args={'n_procs': 2})
-
-
-
+###############################################################################
+# Run workflow locally on 2 CPUs in parrallel
+#main_workflow.run(plugin='MultiProc', plugin_args={'n_procs': 2})
 ################################################################################
 # plotting k_core values
 
 from graphpype.utils_visbrain import visu_graph # noqa
 
-labels_file = op.join(data_path, "correct_channel_names.txt")
-coords_file = op.join(data_path, "MNI_coords.txt")
+labels_file = op.join(data_path, "label_names.txt")
+coords_file = op.join(data_path, "label_centroid.txt")
 
 #labels_file = op.join(data_path, "label_names.txt")
 #coords_file = op.join(data_path, "label_centroid.txt")
@@ -152,7 +181,7 @@ for nf, freq_band_name in enumerate(freq_band_names):
     res_path = op.join(
         data_path, graph_analysis_name,
         "graph_bct_pipe",
-        "_freq_band_name_"+freq_band_name)
+        "_freq_band_name_"+freq_band_name+"_subject_id_sub-0003")
 
     node_k_file = op.join(res_path, "k_core", "coreness.npy")
 
