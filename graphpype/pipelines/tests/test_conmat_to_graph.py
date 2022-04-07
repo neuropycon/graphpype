69d6b412e879a1a2e0ea8eb3dffead3f4444c6f0
David Meunier <david_meunier_79@hotmail.fr>
2022-04-07 13:50:39 +0200

    make_tmp_dir

--- graphpype/pipelines/tests/test_conmat_to_graph.py
+++ graphpype/pipelines/tests/test_conmat_to_graph.py
@@ -3,23 +3,27 @@ import os
 from graphpype.pipelines.conmat_to_graph import (
     create_pipeline_conmat_to_graph_density)
 
-from graphpype.utils_tests import load_test_data
+from graphpype.utils_tests import load_test_data, make_tmp_dir
+
+data_path = load_test_data("data_con")
+
+conmat_file = os.path.join(data_path, "Z_cor_mat_resid_ts.npy")
+
+labels_file = os.path.join(data_path, "ROI_labels-Atlas.txt")
+coords_file = os.path.join(data_path, "ROI_MNI_coords-Atlas.txt")
+
+tmp_dir = make_tmp_dir()
 
 def test_conmat_to_graph_simple():
-    data_path = load_test_data("data_con")
-
-    conmat_file = os.path.join(data_path, "Z_cor_mat_resid_ts.npy")
-
-    labels_file = os.path.join(data_path, "ROI_labels-Atlas.txt")
-    coords_file = os.path.join(data_path, "ROI_MNI_coords-Atlas.txt")
 
     wf = create_pipeline_conmat_to_graph_density(
-        main_path=data_path, pipeline_name="conmat_to_graph_simple")
+        main_path=tmp_dir, pipeline_name="conmat_to_graph_simple")
 
     wf.inputs.inputnode.conmat_file = conmat_file
 
     wf.run()
 
+    assert os.path.exists(os.path.join(tmp_dir, "conmat_to_graph_simple_den_1_0", "graph.png"))
 
 """
 def test_conmat_to_graph_params():
