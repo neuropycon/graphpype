511a395c26cd6ae67a32326982ef41c5d6a7be38
David Meunier <david_meunier_79@hotmail.fr>
2022-04-08 15:39:04 +0200

    only one fonction load_test_data ...

--- graphpype/utils_tests.py
+++ graphpype/utils_tests.py
@@ -98,57 +98,8 @@ def load_test_data(name, path_to=""):
     return data_path
 
 
-
-
-
-def load_test_data(name):
-    """load test data"""
-
-    data_dir = {"data_nii": "RCjt2KioMBmHRxj",
-                "data_nii_mask": "BsjCfxSrXXGEaRD",
-                "data_con": "8mofHzzWAiTZCw8",
-                "data_nii_HCP": "sEPCRnQbs3RFFmw",
-                "data_nii_min": "ypWHfE2kocQ7zq6",
-                # "data_gather": "AfYgQZ87zTGJDx9",
-                "data_con_meg": "Z4iebjw8maLt3Sd",
-                "data_inv_ts": "KWTNYXesE2isZYa"}
-
-    data_dirpath = op.join(op.split(__file__)[0], "..", "data")
-
-    try:
-        os.makedirs(data_dirpath)
-    except OSError:
-        pass
-
-    data_path = op.join(data_dirpath, name)
-    data_zip = op.join(data_dirpath, "{}.zip".format(name))
-
-    if op.exists(data_path):
-        return data_path
-
-    if not op.exists(data_zip):
-
-        assert name in data_dir.keys(),\
-            "Error, {} not found in data_dict".format(name)
-        oc_path = "https://amubox.univ-amu.fr/s/{}/download"\
-            .format(data_dir[name])
-        os.system("wget -O {} --no-check-certificate  --content-disposition\
-            {}".format(data_zip, oc_path))
-
-    assert op.exists(data_zip),\
-        "Error, data_zip = {} not found ".format(data_zip)
-
-    os.system("unzip -o {} -d {}".format(data_zip, data_dirpath))
-    os.remove(data_zip)
-
-    data_path = op.join(data_dirpath, name)
-
-    assert op.exists(data_path)
-
-    return data_path
-
-
 def make_tmp_dir():
+    import shutil
     tmp_dir = "/tmp/test_graphpype"
     if op.exists(tmp_dir):
         shutil.rmtree(tmp_dir)
