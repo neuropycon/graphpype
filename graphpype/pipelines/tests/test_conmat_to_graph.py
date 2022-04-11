3b9f7d4c70b464e9d0fd1ec339754b7cd25dc8df
David Meunier <david_meunier_79@hotmail.fr>
2022-04-11 16:16:20 +0200

    Test GitHub actions (#57)
    Re wrote unit-test and pep8 check, previously defined in travis, to launch on PR in github actions (see previous PR)

--- graphpype/pipelines/tests/test_conmat_to_graph.py
+++ graphpype/pipelines/tests/test_conmat_to_graph.py
@@ -1,27 +1,52 @@
 import os
-"""
+
 from graphpype.pipelines.conmat_to_graph import (
     create_pipeline_conmat_to_graph_density)
-"""
-data_path = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), "..", "..", "tests", "data", "data_con")
 
-conmat_file = os.path.join(data_path, "Z_cor_mat_resid_ts.npy")
+from graphpype.utils_tests import load_test_data
 
-labels_file = os.path.join(data_path, "ROI_labels-Atlas.txt")
-coords_file = os.path.join(data_path, "ROI_MNI_coords-Atlas.txt")
 
-"""
 def test_conmat_to_graph_simple():
+
+    data_path = load_test_data("data_con")
+
+    conmat_file = os.path.join(data_path, "Z_cor_mat_resid_ts.npy")
 
     wf = create_pipeline_conmat_to_graph_density(
         main_path=data_path, pipeline_name="conmat_to_graph_simple")
 
     wf.inputs.inputnode.conmat_file = conmat_file
 
+    wf.write_graph(graph2use="colored")
+
     wf.run()
 
+    assert os.path.exists(os.path.join(data_path,
+                                       "conmat_to_graph_simple_den_1_0",
+                                       "graph.json"))
 
+    assert os.path.exists(os.path.join(data_path,
+                                       "conmat_to_graph_simple_den_1_0",
+                                       "graph.png"))
+
+    assert os.path.exists(os.path.join(data_path,
+                                       "conmat_to_graph_simple_den_1_0",
+                                       "compute_net_List", "Z_List.txt"))
+
+    assert os.path.exists(os.path.join(data_path,
+                                       "conmat_to_graph_simple_den_1_0",
+                                       "prep_rada", "Z_List.net"))
+
+    assert os.path.exists(os.path.join(data_path,
+                                       "conmat_to_graph_simple_den_1_0",
+                                       "community_rada", "Z_List.lol"))
+
+    assert os.path.exists(os.path.join(data_path,
+                                       "conmat_to_graph_simple_den_1_0",
+                                       "node_roles", "node_roles.txt"))
+
+
+"""
 def test_conmat_to_graph_params():
 
     wf = create_pipeline_conmat_to_graph_density(
--- graphpype/utils_tests.py
+++ graphpype/utils_tests.py
@@ -4,58 +4,112 @@ Support function for loading test datasets
 import os
 import os.path as op
 
+import shutil
+import json
+import subprocess
 
-def load_test_data(name):
-    """load test data"""
 
-    data_dir = {"data_nii": "RCjt2KioMBmHRxj",
-                "data_nii_mask": "BsjCfxSrXXGEaRD",
-                "data_con": "8mofHzzWAiTZCw8",
-                "data_nii_HCP": "sEPCRnQbs3RFFmw",
-                "data_nii_min": "ypWHfE2kocQ7zq6",
-                # "data_gather": "AfYgQZ87zTGJDx9",
-                "data_con_meg": "Z4iebjw8maLt3Sd",
-                "data_inv_ts": "KWTNYXesE2isZYa"}
-    """
-    data_dir = {"data_nii": "8HP8YRACDK7nPH7",
-                "data_nii_mask": "pnzeHjz3xGY7Zij",
-                "data_con": "kEXrxPs4DgAJaLs",
-                "data_gather": "5SQpidYQJXqij6F",
-                "data_con_meg": "yAbqiq5WqAzSRkC",
-                "data_inv_ts": "HT85yik8iR5W5rF"}
-    """
-    data_dirpath = op.join(op.split(__file__)[0], "..", "data")
+def _download_data_zip(data_zip, name):
 
-    try:
-        os.makedirs(data_dirpath)
-    except OSError:
-        pass
+    json_data = op.join(op.dirname(op.abspath(__file__)),
+                        "data_test_servers.json")
 
-    data_path = op.join(data_dirpath, name)
-    data_zip = op.join(data_dirpath, "{}.zip".format(name))
+    data_dict = json.load(open(json_data))
 
-    if op.exists(data_path):
-        return data_path
+    for key, cloud_elem in data_dict.items():
+        print(key)
 
-    if not op.exists(data_zip):
+        data_dir = cloud_elem["data_dir"]
 
-        assert name in data_dir.keys(),\
-            "Error, {} not found in data_dict".format(name)
-        oc_path = "https://amubox.univ-amu.fr/s/{}/download"\
-            .format(data_dir[name])
-        # oc_path = "https://cloud.int.univ-amu.fr/index.php/s/{}/download"\
-        #    .format(data_dir[name])
-        os.system("wget -O {} --no-check-certificate  --content-disposition\
-            {}".format(data_zip, oc_path))
+        if name not in data_dir.keys():
+
+            print("{} not found in {}".format(name, key))
+            continue
+
+        server = cloud_elem["server"]
+
+        if "cloud_format" in list(cloud_elem.keys()):
+            oc_path = cloud_elem["cloud_format"].format(server, data_dir[name])
+        elif "cloud_format_3" in list(cloud_elem.keys()):
+            oc_path = cloud_elem["cloud_format_3"].format(server,
+                                                          data_dir[name], name)
+
+        cmd = 'wget --no-check-certificate  \
+            --content-disposition  {} -O {} '.format(oc_path, data_zip)
+
+        val = subprocess.call(cmd.split())
+
+        if val:
+            print("Error with {} for {}".format(cmd, key))
+            continue
+
+        if op.exists(data_zip):
+            print(os.listdir(op.split(data_zip)[0]))
+
+            print("Ok for download {} with {}".format(data_zip, key))
+            print("Quitting download function")
+
+            return True
 
     assert op.exists(data_zip),\
         "Error, data_zip = {} not found ".format(data_zip)
 
-    os.system("unzip -o {} -d {}".format(data_zip, data_dirpath))
-    os.remove(data_zip)
+    return False
+
+
+def load_test_data(name, path_to="", verbose=False):
+    """ Load test data, template and needed scripts """
+
+    if path_to == "":
+        path_to = op.expanduser("~")
+
+    assert op.exists(path_to), "Breaking, {} do not exist".format(path_to)
+
+    data_dirpath = op.join(path_to, "data_graphpype")
+
+    if verbose:
+        print(data_dirpath)
+
+    try:
+        os.makedirs(data_dirpath)
+    except OSError:
+        print("data_dirpath {} already exists".format(data_dirpath))
 
     data_path = op.join(data_dirpath, name)
 
-    assert op.exists(data_path)
+    if op.exists(data_path):
+        print("{} Already exists, skipping download".format(data_path))
+        return data_path
+
+    data_zip = op.join(data_dirpath, "{}.zip".format(name))
+
+    if verbose:
+        print(data_zip)
+
+    if not op.exists(data_zip):
+        print("Download {}".format(data_zip))
+
+        val = _download_data_zip(data_zip, name)
+
+        assert val, "Error, cannot download {}".format(data_zip)
+
+    assert op.exists(data_zip), "Error, cannot find {}".format(data_zip)
+
+    print("Unzip {} to {}".format(data_zip, data_path))
+    os.system("unzip -o {} -d {}".format(data_zip, data_path))
+    os.remove(data_zip)
+
+    assert op.exists(data_path), "Error, cannot find {}".format(data_path)
 
     return data_path
+
+
+def make_tmp_dir():
+
+    tmp_dir = "/tmp/test_graphpype"
+    if op.exists(tmp_dir):
+        shutil.rmtree(tmp_dir)
+    os.makedirs(tmp_dir)
+    os.chdir(tmp_dir)
+
+    return tmp_dir
